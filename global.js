f51c01fc689558c8b4162220b8cf00e09d718281
ruwix <grantpauker@gmail.com>
2018-01-20 23:24:33 -0800

    add getScreen function

--- global.js
+++ global.js
@@ -1,4 +1,4 @@
-const api_key  = "ce4f08aa7cc2cea5"; 
+const api_key = "ce4f08aa7cc2cea5";
 // const forecastURL = "http://api.wunderground.com/api/" + api_key + "/forecast/q/CA/La_jolla.json";
 // const conditionsURL = "http://api.wunderground.com/api/" + api_key + "/conditions/q/CA/La_jolla.json";
 // const hourlyURL = "http://api.wunderground.com/api/" + api_key + "/hourly/q/CA/La_jolla.json";
@@ -13,4 +13,14 @@ const hourly10dayURL = "json/hourly10day.json";
 
 function openScreen(n) {
   window.location.href = "screen_" + n + ".html";
+}
+
+function getScreen() {
+  var n = new String(window.location.pathname).substring(window.location.pathname.lastIndexOf('/') + 1);
+  if (n.lastIndexOf(".") != -1) {
+    n = n.substring(0, n.lastIndexOf("."));
+  }
+  n = n.substring(n.lastIndexOf("_") + 1);
+  n = parseInt(n);
+  return n;
 }
